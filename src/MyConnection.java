1c3f1cee8382df16be853ca998ba691f81ac7034
Kenjie <kbalona5@gmail.com>
2023-05-20 21:55:41 +0800

    first commit

---
+++ src/MyConnection.java
@@ -0,0 +1,19 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MyConnection {
+    
+    public static Connection getConnection() {
+        
+        Connection con = null;
+        try {
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3307/dbonlinebankingsystem", "root", "");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        
+        return con;
+    }
+}
